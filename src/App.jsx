8cd232f72cc7538dade09ed2eed16fc971b0d475
Kais Tarraf <hossamtr184@gmail.com>
2026-01-02 07:03:18 +0300

    wip: begin Benefits section setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
 import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 const App = () => {
-  return (
-    <> 
-      <div className="pt-19 lg:pt-21 overflow-hidden">
-        <Header />
-        <Hero/>
-      </div>
-      <ButtonGradient />
-    </>
-  );
+	return (
+		<>
+			<div className="pt-19 lg:pt-21 overflow-hidden">
+				<Header />
+				<Hero />
+				<Benefits />
+			</div>
+			<ButtonGradient />
+		</>
+	);
 };
 
 export default App;
---
+++ src/components/Benefits.jsx
@@ -0,0 +1,27 @@
+import { benefits } from "../constants";
+import Heading from "./Heading";
+import Section from "./Section";
+
+const Benefits = () => {
+	return (
+		<Section id="features">
+			<div className="container relative z-2">
+				<Heading
+					className="md:max-w-mb lg:max-w-2xl"
+					title="Chat Smarter, Not Harder with Brainwave"
+				/>
+				<div className="flex flex-wrap gap-10 mb-10">
+					{benefits.map((item) => (
+						<div key={item.id}>
+							<div>
+								<h5>{item.title}</h5>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</Section>
+	);
+};
+
+export default Benefits;
