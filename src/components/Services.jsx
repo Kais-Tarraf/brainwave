c7b6ba807f32f6ef2e620fdd1f5285bebce8a9c9
Kais Tarraf <hossamtr184@gmail.com>
2026-01-18 17:10:37 +0300

    finalize services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,6 +4,12 @@ import Heading from "./Heading";
 import { service1, service2, service3, check } from "../assets";
 import { brainwaveServices, brainwaveServicesIcons } from "../constants";
 import Generating from "./Generating";
+import {
+	PhotoChatMessage,
+	Gradient,
+	VideoBar,
+	VideoChatMessage,
+} from "./design/Services";
 const Services = () => {
 	return (
 		<Section id="how-to-use">
@@ -56,6 +62,81 @@ const Services = () => {
 						/>
 					</div>
 				</div>
+				<div className="relative z-1 grid gap-5 lg:grid-cols-2">
+					<div
+						className="relative min-h-156 border 
+					border-n-1/10 rounded-3xl overflow-hidden"
+					>
+						<div className="absolute inset-0">
+							<img
+								src={service2}
+								className="h-ful w-full object-cover"
+								width={630}
+								height={750}
+								alt="robot"
+							/>
+						</div>
+						<div
+							className="absolute inset-0 flex flex-col justify-end
+						p-8 bg-linear-to-b from-n-8/0 to-n-8/90 lg:p-15"
+						>
+							<h4 className="mb-4">Photo editing</h4>
+							<p className="body-2 mb-12 text-n-3">
+								Automatically enhance your photos using our AI app's photo
+								editing feature. Try it now!
+							</p>
+						</div>
+						<PhotoChatMessage />
+					</div>
+					<div className="p-4 bg-n-7 rounded-2xl overflow-hidden lg:min-h-184">
+						<div className="p-12 px-4 xl:px-8">
+							<h4 className="mb-4">Video generation</h4>
+							<p className="body-2 mb-8 text-n-3">
+								The world's most powerful AI photo and video art generation
+								engine. What will you create?
+							</p>
+							<ul className="flex items-center justify-between">
+								{brainwaveServicesIcons.map((item, index) => (
+									<li
+										key={index}
+										className={`rounded-2xl flex items-center justify-center
+											${
+												index === 2
+													? "w-12 h-12 p-0.25 bg-conic md:w-18 md:h-18"
+													: "flex w-10 h-10 bg-n-6 md:w-15 md:h-15"
+											}
+									`}
+									>
+										<div
+											className={
+												index === 2
+													? "flex items-center justify-center w-full h-full bg-n-7 rounded-2xl"
+													: ""
+											}
+										>
+											<img src={item} width={24} height={24} alt={item} />
+										</div>
+									</li>
+								))}
+							</ul>
+						</div>
+						<div
+							className="relative h-48 bg-n-8 rounded-xl
+						overflow-hidden md:h-100"
+						>
+							<img
+								src={service3}
+								className="w-full h-full object-cover"
+								width={520}
+								height={400}
+								alt="Scary robot"
+							/>
+							<VideoChatMessage />
+							<VideoBar />
+						</div>
+					</div>
+				</div>
+				<Gradient />
 			</div>
 		</Section>
 	);
