98ce025f248e9457949e0559287f8124f0e07ad0
Kais Tarraf <hossamtr184@gmail.com>
2026-03-03 15:26:39 +0300

    apply animation on the benefits cards

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -4,7 +4,34 @@ import Section from "./Section";
 import Arrow from "../assets/svg/Arrow";
 import { GradientLight } from "../components/design/Benefits";
 import ClipPath from "../assets/svg/ClipPath";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useRef } from "react";
+gsap.registerPlugin(ScrollTrigger);
 const Benefits = () => {
+	const scrollRef = useRef();
+	useGSAP(
+		() => {
+			gsap.from(".card", {
+				y: 80,
+				opacity: 0,
+				scale: 0.95,
+				duration: 0.8,
+				ease: "power3.out",
+				stagger: {
+					each: 0.2,
+					from: "start",
+				},
+				scrollTrigger: {
+					trigger: scrollRef.current,
+					start: "top 75%",
+					toggleActions: "play none none none",
+				},
+			});
+		},
+		{ scope: scrollRef }
+	);
 	return (
 		<Section id="features">
 			<div className="container relative z-2">
@@ -12,12 +39,15 @@ const Benefits = () => {
 					className="md:max-w-mb lg:max-w-2xl"
 					title="Chat Smarter, Not Harder with Brainwave"
 				/>
-				<div className="flex flex-wrap justify-center gap-10 mb-10">
+				<div
+					ref={scrollRef}
+					className="flex flex-wrap justify-center gap-10 mb-10"
+				>
 					{benefits.map((item) => (
 						<div
 							key={item.id}
 							className="block relative p-0.5
-						bg-no-repeat bg-size-[100%_100%] md:max-w-[24rem]
+						bg-no-repeat bg-size-[100%_100%] md:max-w-[24rem] card
 						"
 							style={{ backgroundImage: `url(${item.backgroundUrl})` }}
 						>
